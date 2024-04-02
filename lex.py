4beef2954843ecfdbf099ef389a93cfafed12558
J-Galvis <galviscamilojuan@gmail.com>
2024-04-02 14:41:52 -0500

    Ya tokeniza

--- lex.py
+++ lex.py
@@ -83,11 +83,17 @@ tokens = (
     '__TRAIT__',
     #symbols de php
     #falta revisar que no falte alguno
-    #?no hay para + o - o ( o ) ¿revisar?
+    #?no hay para + o - ni ,¿revisar?
     'AND_EQUAL',
     'ARRAY_CAST', #!tengo que verla nota
     'ATTRIBUTE',
-    'BAD_CHARACTER',
+    #----------------------------------
+    # Tokens que no maneja php de por si
+    'EQUAL',
+    'RIGHT_PARENTHESIS',
+    'LEFT_PARENTHESIS',
+    'SEMICOLON',
+    #----------------------------------
     'BOOLEAN_AND',
     'BOOLEAN_OR',
     'BOOL_CAST',
@@ -148,429 +154,432 @@ tokens = (
     'XOR_EQUAL', 
 )
 
-se llama lola 
-
 # Reglas de expresiones regulares para tokens simples.
 #?hay que encontrarlas
-T_NS_SEPARATOR = r'\\'
+t_NS_SEPARATOR = r'\\'
+t_EQUAL = r'='
+t_LEFT_PARENTHESIS = r'\('
+t_RIGHT_PARENTHESIS = r'\)'
+t_SEMICOLON = r'\;'
 #palabras reservadas declaracion
 #las que tienen revisar a un lado es porque en la lista aparece con un () y falta decidir como tomar esa exprecion
-def T_HALT_COMPILER(t): #!revisar
+def t_HALT_COMPILER(t): #!revisar
     r'__halt_compiler\(\)'
     return t
-def T_ABSTRACT(t):
+def t_ABSTRACT(t):
     r'abstract'
     return t
-def T_LOGICAL_AND(t):
+def t_LOGICAL_AND(t):
     r'and'
     return t
-def T_ARRAY(t):
+def t_ARRAY(t):
     r'array'
     return t
-def T_AS(t):
+def t_AS(t):
     r'as'
     return t
-def T_BREAK(t):
+def t_BREAK(t):
     r'break'
     return t
-def T_CALLABLE(t):
+def t_CALLABLE(t):
     r'callable'
     return t
-def T_CASE(t):
+def t_CASE(t):
     r'case'
     return t
-def T_CATCH(t):
+def t_CATCH(t):
     r'catch'
     return t
-def T_CLASS(t):
+def t_CLASS(t):
     r'class'
     return t
-def T_CLONE(t):
+def t_CLONE(t):
     r'clone'
     return t
-def T_CONST(t):
+def t_CONST(t):
     r'const'
     return t
-def T_CONTINUE(t):
+def t_CONTINUE(t):
     r'continue'
     return t
-def T_DECLARE(t):
+def t_DECLARE(t):
     r'declare'
     return t
-def T_DEFAULT(t):
+def t_DEFAULT(t):
     r'default'
     return t
-def T_DO(t):
+def t_DO(t):
     r'do'
     return t
-def T_ECHO(t):
+def t_ECHO(t):
     r'echo'
     return t
-def T_ELSE(t):
+def t_ELSE(t):
     r'else'
     return t
-def T_ELSEIF(t):
+def t_ELSEIF(t):
     r'elseif'
     return t
-def T_EMPTY(t):
+def t_EMPTY(t):
     r'empty'
     return t
-def T_ENDDECLARE(t):
+def t_ENDDECLARE(t):
     r'enddeclare'
     return t
-def T_ENDFOR(t):
+def t_ENDFOR(t):
     r'endfor'
     return t
-def T_ENDFOREACH(t):
+def t_ENDFOREACH(t):
     r'endforeach'
     return t
-def T_ENDIF(t):
+def t_ENDIF(t):
     r'endif'
     return t
-def T_ENDSWITCH(t):
+def t_ENDSWITCH(t):
     r'endswitch'
     return t
-def T_ENDWHILE(t):
+def t_ENDWHILE(t):
     r'endwhile'
     return t
-def T_EVAL(t): #!revisar
+def t_EVAL(t): #!revisar
     r'eval'
     return t
-def T_EXIT(t):
+def t_EXIT(t):
     r'exit | die'
     return t
-def T_EXTENDS(t):
+def t_EXTENDS(t):
     r'extends'
     return t
-def T_FINAL(t):
+def t_FINAL(t):
     r'final'
     return t
-def T_FINALLY(t):
+def t_FINALLY(t):
     r'finally'
     return t
-def T_FN(t):
+def t_FN(t):
     r'fn'
     return t
-def T_FOR(t):
+def t_FOR(t):
     r'for'
     return t
-def T_FOREACH(t):
+def t_FOREACH(t):
     r'foreach'
     return t
-def T_FUNCTION(t):
+def t_FUNCTION(t):
     r'function'
     return t
-def T_GLOBAL(t):
+def t_GLOBAL(t):
     r'global'
     return t
-def T_GOTO(t):
+def t_GOTO(t):
     r'goto'
     return t
-def T_IF(t):
+def t_IF(t):
     r'if'
     return t
-def T_IMPLEMENTS(t):
+def t_IMPLEMENTS(t):
     r'implements'
     return t
-def T_INCLUDE(t): #!revisar
+def t_INCLUDE(t): #!revisar
     r'include'
     return t
-def T_INCLUDE_ONCE(t): #!revisa
+def t_INCLUDE_ONCE(t): #!revisa
     r'include_once'
     return t
-def T_INSTANCEOF(t):
+def t_INSTANCEOF(t):
     r'instanceof'
     return t
-def T_INSTEADOF(t):
+def t_INSTEADOF(t):
     r'insteadof'
     return t
-def T_INTERFACE(t):
+def t_INTERFACE(t):
     r'interface'
     return t
-def T_ISSET(t): #!revisa
+def t_ISSET(t): #!revisa
     r'isset'
     return t
-def T_LIST(t): #!revisa
+def t_LIST(t): #!revisa
     r'list'
     return t
-def T_NAMESPACE(t):
+def t_NAMESPACE(t):
     r'namespace'
     return t
-def T_NEW(t):
+def t_NEW(t):
     r'new'
     return t
-def T_LOGICAL_OR(t):
+def t_LOGICAL_OR(t):
     r'or'
     return t
-def T_PRINT(t): #!revisa
+def t_PRINT(t): #!revisa
     r'print\(.*?\)'
     return t
-def T_PRIVATE(t):
+def t_PRIVATE(t):
     r'private'
     return t
-def T_PROTECTED(t):
+def t_PROTECTED(t):
     r'protected'
     return t
-def T_PUBLIC(t):
+def t_PUBLIC(t):
     r'public'
     return t
-def T_REQUIRE(t): #!revisar
+def t_REQUIRE(t): #!revisar
     r'require'
     return t
-def T_REQUIRE_ONCE(t): #!revisar
+def t_REQUIRE_ONCE(t): #!revisar
     r'require_once'
     return t
-def T_RETURN(t):
+def t_RETURN(t):
     r'return'
     return t
-def T_STATIC(t):
+def t_STATIC(t):
     r'static'
     return t
-def T_SWITCH(t):
+def t_SWITCH(t):
     r'switch'
     return t
-def T_THROW(t):
+def t_THROW(t):
     r'throw'
     return t
-def T_TRAIT(t):
+def t_TRAIT(t):
     r'trait'
     return t
-def T_TRY(t):
+def t_TRY(t):
     r'try'
     return t
-def T_UNSET(t): #!revisar
+def t_UNSET(t): #!revisar
     r'unset'
     return t
-def T_USE(t):
+def t_USE(t):
     r'use'
     return t
-def T_VAR(t):
+def t_VAR(t):
     r'var'
     return t
-def T_WHILE(t):
+def t_WHILE(t):
     r'while'
     return t
-def T_LOGICAL_XOR(t):
+def t_LOGICAL_XOR(t):
     r'xor'
     return t
-def T_YIELD(t):
+def t_YIELD(t):
     r'yield'
     return t
-def T_YIELD_FROM(t):
+def t_YIELD_FROM(t):
     r'yield from'
     return t
-def T_CLASS_C(t):
+def t_CLASS_C(t):
     r'__CLASS__'
     return t
-def T_DIR(t):
+def t_DIR(t):
     r'__DIR__'
     return t
-def T_FILE(t):
+def t_FILE(t):
     r'__FILE__'
     return t
-def T_FUNC_C(t):
+def t_FUNC_C(t):
     r'__FUNCTION__'
     return t
-def T_LINE(t):
+def t_LINE(t):
     r'__LINE__'
     return t
-def T_METHOD_C(t):
+def t_METHOD_C(t):
     r'__METHOD__'
     return t
-def T_NS_C(t):
+def t_NS_C(t):
     r'__NAMESPACE__'
     return t
-def T_TRAIT_C(t):
+def t_TRAIT_C(t):
     r'__TRAIT__'
     return t
 #hasta qui las palabras reservadas de php
 #*FAlta la definicion de los simbolos de php
-def T_AND_EQUAL(t):
+def t_AND_EQUAL(t):
     r'&='
     return t
-def T_ARRAY_CAST(t):
+def t_ARRAY_CAST(t):
     r'\(array\)'
     return t
-def T_ATTRIBUTE(t): #!revisar por motivo de la lista 
+def t_ATTRIBUTE(t): #!revisar por motivo de la lista 
     r'\#\[.*?\]'
     return t
-def T_BAD_CHARACTER(t): #!revisar no se como tomarlo por lo que este debajo des ascii 32 y sus excepciones
-    r'.'
-    return t
-def T_BOOLEAN_AND(t):
+# def t_BAD_CHARACTER(t): #!revisar no se como tomarlo por lo que este debajo des ascii 32 y sus excepciones
+#     r'.'
+#     return t
+def t_BOOLEAN_AND(t):
     r'&&'
     return t
-def T_BOOLEAN_OR(t):
+def t_BOOLEAN_OR(t):
     r'\|\|'
     return t
-def T_BOOL_CAST(t):
+def t_BOOL_CAST(t):
     r'\(bool\) | \(boolean\)'
     return t
-def T_CLOSE_TAG(t):
+def t_CLOSE_TAG(t):
     r'\?> | \%\>'
     return t
-def T_COALESCE(t):
+def t_COALESCE(t):
     r'\?\?'
     return t
-def T_COALESCE_EQUAL(t):
+def t_COALESCE_EQUAL(t):
     r'\?\?='
     return t
-def T_COMMENT(t):
-    r'//.*?\n | /\*.*?\*/ | #.*?\n'
+def t_COMMENT(t):
+    r'(//.*?\n | /\*.*?\*/ | \#.*?\n)'
     return t
-def T_CONCAT_EQUAL(t):
+def t_CONCAT_EQUAL(t):
     r'\.='
     return t
-def T_CONSTANT_ENCAPSED_STRING(t): #!revisar la expresion regular
-    r'\'[^\']*\' | \"[^\"]*\"'
+def t_CONSTANT_ENCAPSED_STRING(t): #!revisar la expresion regular
+    r'(\'[^\']*\' | \"[^\"]*\")'
     return t
-def T_CURLY_OPEN(t):
+def t_CURLY_OPEN(t):
     r'\$\{'
     return t
-def T_DEC(t):
+def t_DEC(t):
     r'--'
     return t
-def T_DIV_EQUAL(t):
+def t_DIV_EQUAL(t):
     r'/='
     return t
-def T_DOC_COMMENT(t): #!revisar la expresion regular
-    r'/\*\*(.|\n)*?\*/'
+def t_DOC_COMMENT(t): #!revisar la expresion regular
+    r'(/\*\*(.|\n)*?\*/)'
     return t
-def T_DOLLAR_OPEN_CURLY_BRACES(t):
+def t_DOLLAR_OPEN_CURLY_BRACES(t):
     r'\$\{'
     return t
-def T_DOUBLE_ARROW(t):
+def t_DOUBLE_ARROW(t):
     r'=>'
     return t
-def T_DOUBLE_CAST(t):
+def t_DOUBLE_CAST(t):
     r'\(double\) | \(float\) | \(real\)'
     return t
-def T_DOUBLE_COLON(t):
+def t_DOUBLE_COLON(t):
     r'::'
     return t
-def T_ELLIPSIS(t):
+def t_ELLIPSIS(t):
     r'\.\.\.'
     return t
-def T_DNUMBER(t): #!revisar la expresion regular (es la de la documentacion de php)
+def t_DNUMBER(t): #!revisar la expresion regular (es la de la documentacion de php)
     r'([0-9]*(_[0-9]+)*[\.]{[0-9]+(_[0-9]+)*}) | ({[0-9]+(_[0-9]+)*}[\.][0-9]*(_[0-9]+)*)'
     return t
-def T_ENCAPSED_AND_WHITESPACE(t): #!revisar la expresion regular y significado de este token
+def t_ENCAPSED_AND_WHITESPACE(t): #!revisar la expresion regular y significado de este token
     r'(?<=\")((?:\\\\.|[^"])+)(?=\")(?=\s|$)'
     return t
-def T_END_HEREDOC(t): #!esta no es la expresion regular hay que encontrarla
+def t_END_HEREDOC(t): #!esta no es la expresion regular hay que encontrarla
     r'\?>'
     return t
-def T_INC(t):
+def t_INC(t):
     r'\+\+'
     return t
-def T_INLINE_HTML(t): #!revisar la expresion regular y significado de este token
+def t_INLINE_HTML(t): #!revisar la expresion regular y significado de este token
     r'<\?php\s+(.*?)(?:\?>|$)'
     return t
-def T_INT_CAST(t): #*revisar la expresion regular y significado de este token
+def t_INT_CAST(t): #*revisar la expresion regular y significado de este token
     r'\(int\) | \(integer\)'
     return t
-def T_IS_EQUAL(t):
+def t_IS_EQUAL(t):
     r'=='
     return t
-def T_IS_GREATER_OR_EQUAL(t):
+def t_IS_GREATER_OR_EQUAL(t):
     r'>='
     return t
-def T_IS_IDENTICAL(t):
+def t_IS_IDENTICAL(t):
     r'==='
     return t
-def T_IS_NOT_EQUAL(t):
+def t_IS_NOT_EQUAL(t):
     r'!= | <>'
     return t
-def T_IS_NOT_IDENTICAL(t):
+def t_IS_NOT_IDENTICAL(t):
     r'!=='
     return t
-def T_IS_SMALLER_OR_EQUAL(t):
+def t_IS_SMALLER_OR_EQUAL(t):
     r'<='
     return t
-def T_LNUMBER(t): #!revisar la expresion regular (es la de la documentacion de php)
+def t_LNUMBER(t): #!revisar la expresion regular (es la de la documentacion de php)
     r'([+-]?(([1-9][0-9]* | 0) | 0[0-7]+ | 0[xX][0-9a-fA-F]+ | 0b[01]+))'
     return t
-def T_MINUS_EQUAL(t):
+def t_MINUS_EQUAL(t):
     r'-='
     return t
-def T_MOD_EQUAL(t):
+def t_MOD_EQUAL(t):
     r'%='
     return t
-def T_MUL_EQUAL(t):
+def t_MUL_EQUAL(t):
     r'\*='
     return t
-def T_NUM_STRING(t): #!revisar la expresion regular y significado de este token
-    r'\[(?<indices>\d+(?:,\d+)*)\]'
+def t_NUM_STRING(t): #!revisar la expresion regular y significado de este token
+    r'\[(\?<indices>\d+(?:,\d+)*)\]'
     return t
-def T_OBJECT_CAST(t): #*revisar la expresion regular y significado de este token
+def t_OBJECT_CAST(t): #*revisar la expresion regular y significado de este token
     r'\(object\)'
     return t
-def T_OBJECT_OPERATOR(t):
+def t_OBJECT_OPERATOR(t):
     r'->'
     return t
-def T_NULLSAFE_OBJECT_OPERATOR(t):
+def t_NULLSAFE_OBJECT_OPERATOR(t):
     r'\?->'
     return t
-def T_OPEN_TAG(t):
+def t_OPEN_TAG(t):
     r'<\?php | <% | <\?'
     return t
-def T_OPEN_TAG_WITH_ECHO(t):
+def t_OPEN_TAG_WITH_ECHO(t):
     r'<\?= | <%='
     return t
-def T_OR_EQUAL(t):
+def t_OR_EQUAL(t):
     r'\|='
     return t
-def T_PAAMAYIM_NEKUDOTAYIM(t): # todo: esta se repite con T_DOUBLE_COLON revisar
+def t_PAAMAYIM_NEKUDOTAYIM(t): # todo: esta se repite con T_DOUBLE_COLON revisar
     r'::'
     return t
-def T_PLUS_EQUAL(t):
+def t_PLUS_EQUAL(t):
     r'\+='
     return t
-def T_POW(t):
+def t_POW(t):
     r'\*\*'
     return t
-def T_POW_EQUAL(t):
+def t_POW_EQUAL(t):
     r'\*\*='
     return t
-def T_SL(t):
+def t_SL(t):
     r'<<'
     return t
-def T_SL_EQUAL(t):
+def t_SL_EQUAL(t):
     r'<<='
     return t
-def T_SPACESHIP(t):
+def t_SPACESHIP(t):
     r'<=>'
     return t
-def T_SR(t):
+def t_SR(t):
     r'>>'
     return t
-def T_SR_EQUAL(t):
+def t_SR_EQUAL(t):
     r'>>='
     return t
-def T_START_HEREDOC(t): #todo mor revisar heredoc
+def t_START_HEREDOC(t): #todo mor revisar heredoc
     r'<<<'
     return t
-def T_STRING(t): #!revisar la expresion regular
+def t_STRING(t): #!revisar la expresion regular
     r'([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)'
     return t
-def T_STRING_CAST(t): #*revisar la expresion regular y significado de este token
+def t_STRING_CAST(t): #*revisar la expresion regular y significado de este token
     r'\(string\)'
     return t
-def T_STRING_VARNAME(t): #!revisar la expresion regular y significado de este token
-    r'\$\{(?<name>[a-zA-Z_][a-zA-Z0-9_]*)\}'
+def t_STRING_VARNAME(t): #!revisar la expresion regular y significado de este token
+    r'\$\{\(?<name>[a-zA-Z_][a-zA-Z0-9_]*\)\}'
     return t
-def T_VARIABLE(t): #!revisar la expresion regular
+def t_VARIABLE(t): #!revisar la expresion regular
     r'\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*'
     return t
-def T_WHITESPACE(t): #*revisar la expresion regular (es la de la documentacion de php)
+def t_WHITESPACE(t): #*revisar la expresion regular (es la de la documentacion de php)
     r'\t | \n | \r'
     return t
-def T_XOR_EQUAL(t):
+def t_XOR_EQUAL(t):
     r'\^='
     return t
 
 # # Ignorar espacios y tabs.
-# t_ignore = r'\t |\r |\n'
+
+t_ignore = r'\t |\r |\n'
 
 
 
